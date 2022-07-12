9e515bf2706eb9e32f8280de1ebd11967833c0fd
zeroprotec <zero.protec@gmail.com>
2022-07-11 22:41:00 -0500

    Correcion de codigo

--- Bodega.java
+++ Bodega.java
@@ -4,24 +4,38 @@ import java.util.List;
 
 public class Bodega {
  // Creacion de Lista
-List<Producto> ArrayList;
+ private List<Producto> listaProductos;
 
-public void agregarProducto(Producto p) {
-    // To Do implement here
+ public void agregarProducto(Producto p) 
+ {
+     // TODO implement here
+ }
+
+public void eliminarProducto(String codigo) 
+ {
+     // TODO implement here
+ }
+
+ public Producto buscarProducto(String criterio) 
+ {
+     // TODO implement here
+     return null;
+ }
+
+ public void modificarPrecio(String codigo, int precio) 
+ {
+     // TODO implement here
+ }
+
+ public void incrementarCantProducto(String codigo, int cant) 
+ {
+     // TODO implement here
+ }
+
+ public void disminuirCantProducto(String codigo, int cant) 
+ {
+     // TODO implement here
+ }
 
 }
 
-public void eliminarProducto(String codigo) {
-        // TODO implement here
-
-}
-public Producto buscarProducto(String criterio) {
-        // TODO implement here
-        return null;
-}
-public void modificarPrecio(String codigo, int precio) {
-        // TODO implement here
-
-}
-
-}
